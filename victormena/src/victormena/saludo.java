c844a368c383bba1dd42107abcff8c4cff130f67
Victor <Victor@Mena>
2021-05-25 12:36:51 +0200

    Nueva clase

---
+++ victormena/src/victormena/saludo.java
@@ -0,0 +1,11 @@
+package victormena;
+
+public class saludo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola soy Victor Mena");
+	}
+
+}
