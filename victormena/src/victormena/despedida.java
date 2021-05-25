fdf143f2afb3b29326080b28a10cf1784275be43
Victor <Victor@Mena>
2021-05-25 12:50:18 +0200

    Nueva rama

---
+++ victormena/src/victormena/despedida.java
@@ -0,0 +1,11 @@
+package victormena;
+
+public class despedida {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola soy Victor Mena");
+	}
+
+}
